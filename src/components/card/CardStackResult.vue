720d9570a6d0671f20ecfa54f239ac68d8694e20
generalsaku <josefgdev@gmail.com>
2024-11-23 18:03:12 +0100

    A little more css

--- src/components/card/CardStackResult.vue
+++ src/components/card/CardStackResult.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="card-stack-result">
     <CardCombined v-for="(card) of cardStackStore.stack" :key="card.record.sort_index" :stack-card="card" :class="{ 'pulsate-outline-success correct': card.correct && card.english }">
       <template v-slot:english>
@@ -9,9 +10,10 @@
         <div :class="{ 'card-interface': true }" @pointerup="() => flip(card)"></div>
       </template>
     </CardCombined>
+    <div style="margin-top: -8px"><span style="text-transform: uppercase;">{{cardStackStore.stack.filter(x => x.correct).length}} / {{ cardStackStore.stack.length }} completed</span></div>
 
     <div class="card-message">(tap to flip card)</div>
-    <button @pointerup="() => jlptSetsStore.reset()">RETURN <BsFillHouseFill style="width: 20px; margin-left: 4px;" /></button>
+    <div><span style="text-transform: uppercase;">Press the <BsFillHouseFill style="width: 16px; vertical-align: middle; margin-top: -4px;" /> button to go back.</span></div>
   </div>
 </template>
 
@@ -20,11 +22,9 @@ import { onMounted } from 'vue';
 import { BsFillHouseFill } from 'vue-icons-plus/bs'
 import { useCardStackStore } from '@/stores/useCardStackStore'
 import type { cardStackCard } from '@/stores/useCardStackStore'
-import { useJLPTSetsStore } from '@/stores/useJLPTSetsStore';
 import CardCombined from './CardCombined.vue'
 
 const cardStackStore = useCardStackStore()
-const jlptSetsStore = useJLPTSetsStore()
 
 onMounted(() => {
   cardStackStore.stack.forEach((card) => {
