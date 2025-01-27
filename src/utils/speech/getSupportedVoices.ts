445507d50eddc0df3f31d8a146a632093fdee6c5
generalsaku <josefgdev@gmail.com>
2025-01-27 15:25:28 +0100

    load

--- src/utils/speech/getSupportedVoices.ts
+++ src/utils/speech/getSupportedVoices.ts
@@ -1,9 +1,11 @@
 export const getSupportedVoices = new Promise<SpeechSynthesisVoice[]>((resolve) => {
-  window.speechSynthesis.addEventListener('voiceschanged', () => {
-    const voices = window.speechSynthesis.getVoices()
-    resolve(voices)
-  })
-  window.speechSynthesis.getVoices()
+  window.addEventListener('load', () => {
+    window.speechSynthesis.addEventListener('voiceschanged', () => {
+      const voices = window.speechSynthesis.getVoices()
+      resolve(voices)
+    })
+    window.speechSynthesis.getVoices()
+  }, false)
 })
 
 export const getSupportedJapaneseVoice = async () =>  (await getSupportedVoices).find(voice => voice.lang === 'ja-JP' || voice.lang === 'ja_JP')
