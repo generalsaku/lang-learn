35fe391cc9937f2aef7176772f52830a3af7869c
generalsaku <josefgdev@gmail.com>
2025-04-23 12:18:33 +0200

    Extended romaji version for detect translation

--- src/utils/translation/cleanTranslationVariables.ts
+++ src/utils/translation/cleanTranslationVariables.ts
@@ -1,5 +1,6 @@
 import type { LLRecord } from "@/types"
 import { toHiragana, toRomaji } from "wanakana"
+import { toRomaji as toRomajiLiteral } from "@/utils/romaji"
 
 export const cleanTranslationVariables = (record: LLRecord, translation: string) => {
 
@@ -11,8 +12,9 @@ export const cleanTranslationVariables = (record: LLRecord, translation: string)
   romaji = cleanText(romaji)
   const translationHiragana = cleanText(toHiragana(translation))
   const translationRomaji = cleanText(toRomaji(translation))
+  const translationRomajiLiteral = cleanText(toRomajiLiteral(translation))
 
-  return { expression, reading, hiragana, romaji, translation, translationHiragana, translationRomaji }
+  return { expression, reading, hiragana, romaji, translation, translationHiragana, translationRomaji, translationRomajiLiteral }
 }
 
 const cleanText = (word: string) => word.replace('ー', '').toLowerCase().trim()
--- src/utils/translation/isTranslationOK.ts
+++ src/utils/translation/isTranslationOK.ts
@@ -4,14 +4,14 @@ import { romajiSoundSimilar } from "@/utils/translation/romajiSimilarSound"
 import { cleanTranslationVariables } from "@/utils/translation/cleanTranslationVariables"
 
 export const isTranslationOK = (record: LLRecord, translatedText: string) => {
-  const { expression, reading, hiragana, romaji, translation, translationHiragana, translationRomaji  } = cleanTranslationVariables(record, translatedText)
+  const { expression, reading, hiragana, romaji, translation, translationHiragana, translationRomaji, translationRomajiLiteral  } = cleanTranslationVariables(record, translatedText)
 
-  const isValid = translation === expression || translation === reading || translationHiragana === hiragana || translationRomaji === romaji
+  const isValid = translation === expression || translation === reading || translationHiragana === hiragana || translationRomaji === romaji || translationRomajiLiteral === romaji
   if (isValid) return true
 
   const { findRecordFromWord }  = useJLPTSetsStore()
   const similarWord = findRecordFromWord(translation)
   if (similarWord && similarWord.hiragana === hiragana) return true
 
-  return romajiSoundSimilar(translationRomaji, romaji)
+  return romajiSoundSimilar(translationRomaji, romaji) || romajiSoundSimilar(translationRomajiLiteral, romaji)
 }
